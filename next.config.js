929956d16e814ab41bd87944ecad772051017777
juaneth <euanwalker2008@gmail.com>
2024-07-10 21:17:15 +0100

    meowskies!

--- next.config.js
+++ next.config.js
@@ -2,19 +2,27 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await
-import ("./src/env.js");
+await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
-    images: {
-        remotePatterns: [{
-            protocol: 'https',
-            hostname: '**',
-            port: '',
-            pathname: '**',
-        }, ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 export default config;
